839d7692a469f88ddb01bc6edca7c858194f8f99
hngd-mn <bakalarz.jakub@gmail.com>
2024-04-09 17:45:30 +0200

    + added system parameter for path so other txt files can be read

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 def main():
-    with open("books/frankenstein.txt") as book:
+    import sys
+    file=sys.argv[1] #using launch parameter
+    import argparse
 
-        
-    
+    parser=argparse.ArgumentParser(description='''Help for wordcounter: ''')
+    parser.add_argument('path/file.txt', nargs='*', default=[1, 2, 3], help='Path to a file that needs to have its words and letters counted')
+    args=parser.parse_args()
+
+    with open(file) as book:
         file_contents = book.read()
         word_count = file_contents.split()
         print("--- Begin report of", book.name, " ---")
         print(len(word_count), " - Words found in the book")
  
-
         lowered_text = file_contents.lower() #converts txt to small letters
-
         dict_of_letters = {}
         for character in lowered_text:
             letter = character.isalpha() #checks if character is a letter
@@ -28,8 +31,8 @@ def main():
         list_of_letters.sort(reverse=True, key=sort_on)
 
         for let_in_list in list_of_letters:
-            print("The letter", let_in_list['Name'], "was found", let_in_list['Count'], "times")
-    print("--- End report ---")
+                print("The letter", let_in_list['Name'], "was found", let_in_list['Count'], "times")
+        print("--- End report ---")
 
 
 main()
